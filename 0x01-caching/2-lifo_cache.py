df6bf52c4e0bbb854bb8bd710d0f4fba1ae628ba
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-22 23:53:29 +0100

    Create 2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" LIFO caching """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    ''' First In First Out Cache System '''
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        ''' Add an item to the cache'''
+        if key and item:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                keys = [key for key in self.cache_data.keys()]
+                del self.cache_data[keys[-1]]
+                print(f"DISCARD: {keys[-1]}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''get an item with specified key from cache'''
+        return self.cache_data.get(key)
