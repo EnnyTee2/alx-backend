a31c376c4c8e736c2f1e2a10cd451727ff627f29
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-22 23:08:32 +0100

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -15,6 +15,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         ''' get method for retrieving item stored in key '''
-        if key is None or key not in self.cache_data.keys():
-            return None
         return self.cache_data.get(key)
