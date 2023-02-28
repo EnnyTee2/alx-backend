d4d41b7e7d4b3f83a0ee8e19536a61a916ce3ac8
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-28 18:27:55 +0100

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,8 +27,8 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """
-    determines and returns the best language 
-    match based on supported languages
+    Determine and return the best language
+    match based on the supported languages list
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
